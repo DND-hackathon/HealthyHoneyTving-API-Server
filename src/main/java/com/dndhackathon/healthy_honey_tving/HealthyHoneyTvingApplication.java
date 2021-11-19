6522b895759dfa431ab50f599b677bc3d1631010
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-11-19 23:27:42 +0900

    gradle & spring boot 환경 구성

---
+++ src/main/java/com/dndhackathon/healthy_honey_tving/HealthyHoneyTvingApplication.java
@@ -0,0 +1,13 @@
+package com.dndhackathon.healthy_honey_tving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyHoneyTvingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyHoneyTvingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dndhackathon/healthy_honey_tving/HealthyHoneyTvingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dndhackathon.healthy_honey_tving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyHoneyTvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
